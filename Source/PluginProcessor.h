c6ba6905ad35d5e49223382c18fd7e57656311af
MeetCoding <meet.prakash.shah@gmail.com>
2025-12-29 11:34:48 +0530

    feat: setup audio processing chains

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -6,6 +6,10 @@
   ==============================================================================
 */
 
+// Note that we use floats and not doubles throught this project
+// as it cuts the memory bandwidth in half 
+// float: 4 bytes, double: 8 bytes
+
 #pragma once
 
 #include <JuceHeader.h>
@@ -58,7 +62,21 @@ public:
     static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
     juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters",createParameterLayout()};
 
+    // juce::dsp processes only mono audio, as we are dealing with stereo,
+    // we will have to duplicate everything for the two channels
+
 private:
+
+    // Declaring type aliases for better readability
+
+    // A filter slope is by default 12, so in order to make a 48db/Oct filter
+    // We pass the Processor context through a chain of four filters
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    
+    MonoChain leftChain, rightChain;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
